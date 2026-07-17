a50de9adf41f85cefdaed6277afe70f1dbbc0e6f
chicorina0219 <katsuri0219@icloud.com>
2026-07-17 12:07:59 +0900

    Update create_structure.sh

--- 04_shell_script/create_structure.sh
+++ 04_shell_script/create_structure.sh
@@ -3,5 +3,5 @@ mkdir -p project/{results,scripts,data}
 touch project/results/output.dat
 touch project/scripts/process.sh
 for i in $(seq -f "%04g" 1 99); do
-  touch project/data/input$i.det
+  touch project/data/input$i.dat
 done
